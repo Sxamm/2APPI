b1fdd283ab3057d23865dd3ac7f6610a1cd9bd31
Sxamm <samuelagudelogiraldo2003@gmail.com>
2023-06-26 00:10:37 -0500

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,117 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.get('/api/cuentas', (req, res) => {
+  // Lógica para obtener la lista de cuentas
+  res.json({ cuentas: [] });
+});
+
+app.post('/api/cuentas', (req, res) => {
+  // Lógica para crear una nueva cuenta
+  const nuevaCuenta = req.body;
+  res.json({ mensaje: 'Cuenta creada exitosamente', cuenta: nuevaCuenta });
+});
+
+// Añade aquí las otras rutas y funcionalidades de tu plataforma bancaria
+app.get('/api/cuentas/:id/saldo', (req, res) => {
+    const cuentaId = req.params.id;
+    // Lógica para obtener el saldo de la cuenta
+    res.json({ saldo: 1000 });
+  });
+  app.post('/api/transferencias', (req, res) => {
+    // Lógica para realizar la transferencia entre cuentas
+    const { cuentaOrigen, cuentaDestino, monto } = req.body;
+    // Realizar la transferencia y actualizar los saldos
+    res.json({ mensaje: 'Transferencia realizada exitosamente' });
+  });
+  app.get('/api/clientes/:id', (req, res) => {
+    const clienteId = req.params.id;
+    // Lógica para obtener los datos del cliente
+    res.json({ cliente: { nombre: 'John Doe', email: 'john.doe@example.com' } });
+  });
+  app.put('/api/clientes/:id', (req, res) => {
+    const clienteId = req.params.id;
+    const datosActualizados = req.body;
+    // Lógica para actualizar los datos del cliente
+    res.json({ mensaje: 'Datos de cliente actualizados correctamente' });
+  });
+  app.get('/api/sucursales', (req, res) => {
+    // Lógica para obtener la lista de sucursales
+    res.json({ sucursales: [] });
+  });
+  app.post('/api/sucursales', (req, res) => {
+    // Lógica para crear una nueva sucursal
+    const nuevaSucursal = req.body;
+    res.json({ mensaje: 'Sucursal creada exitosamente', sucursal: nuevaSucursal });
+  });
+  app.get('/api/empleados/:id', (req, res) => {
+    const empleadoId = req.params.id;
+    // Lógica para obtener los datos del empleado
+    res.json({ empleado: { nombre: 'Jane Smith', puesto: 'Cajero' } });
+  });
+  app.get('/api/cuentas/:id/estado-cuenta', (req, res) => {
+    const cuentaId = req.params.id;
+    // Lógica para obtener el estado de cuenta de la cuenta
+    res.json({ estadoCuenta: [] });
+  });
+  app.post('/api/depositos', (req, res) => {
+    // Lógica para realizar un depósito en una cuenta
+    const { cuenta, monto } = req.body;
+    // Realizar el depósito y actualizar el saldo
+    res.json({ mensaje: 'Depósito realizado exitosamente' });
+  });
+  app.post('/api/retiros', (req, res) => {
+    // Lógica para realizar un retiro de una cuenta
+    const { cuenta, monto } = req.body;
+    // Realizar el retiro y actualizar el saldo
+    res.json({ mensaje: 'Retiro realizado exitosamente' });
+  });
+  app.get('/api/tasa-cambio', (req, res) => {
+    // Lógica para obtener la tasa de cambio actual
+    res.json({ tasaCambio: 1.5 });
+  });
+  app.post('/api/prestamos', (req, res) => {
+    // Lógica para solicitar un préstamo
+    const { cliente, monto } = req.body;
+    // Procesar la solicitud de préstamo y generar una respuesta
+    res.json({ mensaje: 'Solicitud de préstamo enviada correctamente' });
+  });
+  app.post('/api/prestamos/:id/pagos', (req, res) => {
+    const prestamoId = req.params.id;
+    const { monto } = req.body;
+    // Lógica para realizar un pago de un préstamo
+    // Actualizar el saldo y la información del préstamo
+    res.json({ mensaje: 'Pago realizado correctamente' });
+  });
+  app.get('/api/tarjetas/:id', (req, res) => {
+    const tarjetaId = req.params.id;
+    // Lógica para obtener los datos de una tarjeta de crédito
+    res.json({ tarjeta: { numero: '**** **** **** 1234', tipo: 'Visa' } });
+  });
+  app.post('/api/pagos', (req, res) => {
+    // Lógica para realizar un pago con tarjeta de crédito
+    const { tarjeta, monto } = req.body;
+    // Procesar el pago y generar una respuesta
+    res.json({ mensaje: 'Pago realizado exitosamente' });
+  });
+  app.put('/api/tarjetas/:id/bloquear', (req, res) => {
+    const tarjetaId = req.params.id;
+    // Lógica para bloquear una tarjeta de crédito
+    // Actualizar el estado de la tarjeta en la base de datos
+    res.json({ mensaje: 'Tarjeta bloqueada correctamente' });
+  });
+  app.get('/api/cajeros/:id/consulta-saldo/:cuenta', (req, res) => {
+    const cajeroId = req.params.id;
+    const cuenta = req.params.cuenta;
+    // Lógica para realizar una consulta de saldo en un cajero automático
+    res.json({ saldo: 5000 });
+  });
+                                  
+// Inicia el servidor
+app.listen(3000, () => {
+  console.log('Servidor iniciado en http://localhost:3000');
+});
